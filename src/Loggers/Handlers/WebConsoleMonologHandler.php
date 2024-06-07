2d1af87af002589a70982f908d6e1b8212d7a037
Goncalo Martins <goncalo.martins@impactwave.com>
2024-06-07 10:50:44 +0100

    Declaration must be compatible with

--- src/Loggers/Handlers/WebConsoleMonologHandler.php
+++ src/Loggers/Handlers/WebConsoleMonologHandler.php
@@ -4,22 +4,25 @@ namespace PhpKit\WebConsole\Loggers\Handlers;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
 use PhpKit\WebConsole\DebugConsole\DebugConsole;
+use Monolog\LogRecord;
 
 class WebConsoleMonologHandler extends AbstractProcessingHandler
 {
-  public function __construct ($level = Logger::DEBUG, $bubble = true)
-  {
-    parent::__construct ($level, $bubble);
-  }
 
-  protected function write (array $record)
-  {
-    if (DebugConsole::$initialized) {
-      $channel  = $record['channel'];
-      $loggerId = DebugConsole::hasLogger ($channel) ? $channel : DebugConsole::$settings->defaultLoggerId;
-      $logger   = DebugConsole::logger ($loggerId);
-      $logger->log ($record['level'], $record['message'], $record['context']);
-    }
-  }
+	public function __construct($level = Logger::DEBUG, $bubble = true)
+	{
+		parent::__construct($level, $bubble);
+	}
+
+	protected function write(LogRecord $record): void
+	{
+		if (DebugConsole::$initialized)
+		{
+			$channel = $record['channel'];
+			$loggerId = DebugConsole::hasLogger($channel) ? $channel : DebugConsole::$settings->defaultLoggerId;
+			$logger = DebugConsole::logger($loggerId);
+			$logger->log($record['level'], $record['message'], $record['context']);
+		}
+	}
 
 }
